ae4af88d6c257dfb82c1542d8fbbd7b0fc2987d4
Zab528 <ljg6414@gmail.com>
2025-11-17 11:25:45 +0900

    auto: save 2025-11-17 11:25:45 c:\Users\KDT\Desktop\KDT_10SL\EX_MYSQL\DAY1117\ex_select_04.sql

--- EX_MYSQL/DAY1117/ex_select_04.sql
+++ EX_MYSQL/DAY1117/ex_select_04.sql
@@ -1,17 +1,30 @@
 -- ========================================================================
+use mydb;
+
 -- hometown 값 종류
-select DISTINCT * from hometown
+select DISTINCT hometown from user;
 
 -- hometown 기준으로 그룹화
+select hometown, count(hometown)
+from user
+group by hometown;
 
 
 -- 그룹별 행 수
-
+select hometown, count(hometown)
+from user
+group by hometown;
 
 -- ========================================================================
 -- GROUP BY 컬럼명1 , 컬럼명2, .. : 여러 개 컬럼으로 그룹화
 -- ========================================================================
 -- hometown, gender 그룹화
+select hometown, gender
+from user
+group by hometown, gender
 
 
 -- 그룹별 행 수, gender 정렬
+select hometown, gender, count(gender)
+from user
+group by hometown, gender, 
