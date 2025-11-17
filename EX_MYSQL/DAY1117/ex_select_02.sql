cf042fb4baed6319cd592875294d37a755c5b2ee
Zab528 <ljg6414@gmail.com>
2025-11-17 10:42:18 +0900

    auto: save 2025-11-17 10:42:18 c:\Users\KDT\Desktop\KDT_10SL\EX_MYSQL\DAY1117\ex_select_02.sql

--- EX_MYSQL/DAY1117/ex_select_02.sql
+++ EX_MYSQL/DAY1117/ex_select_02.sql
@@ -13,9 +13,37 @@ desc film;
 -- GROUP BY 컬럼명1 , 컬럼명2
 -- ========================================================================
 -- speical features 값 종류
-select DISTINCT speical_features  from film;
+select DISTINCT special_features from film;
 
 -- special features 기준으로 그룹화
-select special_features from film
+select special_features
 from film
 group by special_features;
+
+
+
+select special_features, count(special_features)
+from film
+group by special_features;
+
+
+-- GROUP BY 컬럼명1 , 컬럼명2, .. : 여러 개 컬럼으로 그룹화
+-- ========================================================================
+-- special features, rating
+select DISTINCT rating
+from film;
+
+-- special features, rating 그룹화
+select special_features, rating, count(rating)
+from film
+group by special_features, rating;
+
+-- special features, rating 그룹별 소속된 행수
+select special_features, rating, rating
+from film
+group by special_features, rating;
+
+-- rating, special features 그룹화
+select special_features, rating, count(rating)
+from film
+group by rating, special_features;
