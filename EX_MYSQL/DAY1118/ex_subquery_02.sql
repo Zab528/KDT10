052fe8f2dff28c25b0ff65330c8d8ccec528b8d0
Zab528 <ljg6414@gmail.com>
2025-11-18 11:48:22 +0900

    auto: save 2025-11-18 11:48:22 c:\Users\KDT\Desktop\KDT_10SL\EX_MYSQL\DAY1118\ex_subquery_02.sql

--- EX_MYSQL/DAY1118/ex_subquery_02.sql
+++ EX_MYSQL/DAY1118/ex_subquery_02.sql
@@ -39,3 +39,20 @@ where customer_id > ANY (
     select customer_id from customer
     where first_name in ('ROSA', 'ANA')
 );
+
+
+-- 연산자 EXISTS : 서브쿼리 결과 행이 있으면 True/ 없으면 False
+-- 반환되는 행 존재 여부만 관심. 실제 반환되는 데이터 관심 X. 사용 X
+-- 서브쿼리의 SELECT * 또는 SELECT 1 형식으로 작성하는 경우 대부분
+
+select * from customer
+where EXISTS (
+    select customer_id from customer
+    where first_name in ('KANG')
+);
+
+select 7 from customer
+where first_name in ('ROSA', 'ANA')
+
+select 7 from customer
+where first_name in ('KANG')
