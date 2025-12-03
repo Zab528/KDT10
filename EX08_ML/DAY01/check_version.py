34d02637de61bbe59d806cc9d715998eda27dccd
Zab528 <ljg6414@gmail.com>
2025-12-03 11:40:08 +0900

    auto: save 2025-12-03 11:40:08 c:\Users\KDT\Desktop\KDT_10SL\EX08_ML\DAY01\check_version.py

---
+++ EX08_ML/DAY01/check_version.py
@@ -0,0 +1,15 @@
+## --------------------------------------------------
+### ML 가상환경 설치 패키지 버전 확인
+## --------------------------------------------------
+import pandas as pd
+import sklearn as sk
+import numpy as np
+import matplotlib as ma
+import seaborn as sns
+
+
+print(f'pandas      v.{pd.__version__}')
+print(f'scikitlearn v.{sk.__version__}')
+print(f'numpy       v.{np.__version__}')
+print(f'matplotlib  v.{ma.__version__}')
+print(f'seaborn     v.{sns.__version__}')
